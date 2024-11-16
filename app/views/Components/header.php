8e587ac2ae3f8139d6c5500f5897f24f1ceb7b64
Thisu2002 <thisumevanya@gmail.com>
2024-11-16 21:14:44 +0530

    doctor dashboard improvements

--- app/views/Components/header.php
+++ app/views/Components/header.php
@@ -1,29 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo URLROOT;?>./assets/css/components/navbar.css">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>./assets/css/pages/homepage.css">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>./assets/css/components/doctorcard.css">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>./assets/css/pages/appoinment.css">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>./assets/css/pages/channel.css">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>/assets/css/components/timeslot.css">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>/assets/css/pages/setappoinment.css">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>/assets/css/pages/patientregister.css">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>/assets/css/pages/phdashboard.css">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>/assets/css/pages/phprescription.css">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>./assets/css/pages/Patientregister.css">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>./assets/css/pages/admindashboard.css">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>./assets/css/pages/drDashboard.css">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>./assets/css/fonts.css">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>./assets/css/pages/drViewAppointments.css">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>./assets/css/pages/drAppointment.css">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>./assets/css/pages/drAvailability.css">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>./assets/css/pages/drLabTests.css">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>./assets/css/pages/drMedicalHistory.css">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>./assets/css/pages/drPrescription.css">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>./assets/css/pages/drProfile.css">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>./assets/css/components/navbar.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>./assets/css/pages/homepage.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>./assets/css/components/doctorcard.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>./assets/css/pages/appoinment.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>./assets/css/pages/channel.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/assets/css/components/timeslot.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/assets/css/pages/setappoinment.css?v=<?php echo time(); ?>">
+
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>./assets/css/pages/patientregister.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/assets/css/pages/Patientprofile.css?v=<?php echo time(); ?>">
+
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/assets/css/pages/phdashboard.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>./assets/css/pages/phprescription.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/assets/css/pages/admindashboard.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>./assets/css/pages/drDashboard.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>./assets/css/fonts.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>./assets/css/pages/drViewAppointments.css?v=<?php echo time(); ?>">
+
+
+
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>./assets/css/pages/drAppointment.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="<?php echo URLROOT;?>./assets/css/components/AdminNavbar.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>./assets/css/pages/AdminDrRegister.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>./assets/css/pages/drProfile.css?v=<?php echo time(); ?>">
 </head>
 <body>
+
     
+
+</head>
+
+<body>
