46f8dc30e2c8be28e6fc216e25e7605ec87871b5
Thisu2002 <thisumevanya@gmail.com>
2024-11-20 14:33:09 +0530

    changes to the doctor's navbar

--- app/views/Components/drNavbar.php
+++ app/views/Components/drNavbar.php
@@ -14,15 +14,40 @@
         <img src="<?php echo URLROOT;?>/assets/images/12345.png"  class="drlogo">
     </a>
     
-    <a href="#" class="drNavitems">Dashboard</a>
-    <a href="#" class="drNavitems">Update Availability</a>
-    <a href="#" class="drNavitems">View Appointments</a>
+    <a href="<?php echo URLROOT;?>/drDashboard" class="drNavitems">Dashboard</a>
+    <a href="<?php echo URLROOT;?>/drAvailability" class="drNavitems">Update Availability</a>
+    <a href="<?php echo URLROOT;?>/drViewAppointments" class="drNavitems">View Appointments</a>
 
-    <a href="#">
-        <img src="<?php echo URLROOT;?>/assets/images/loginlogo.jpg"  class="drloginlogo">
-    </a>
-    <a href="#" class="drlogin">Login</a>
+    <div class="dr-profile-dropdown">
+        <a onclick="toggleDropdown()" class="dr-profile-link">
+            <img src="<?php echo URLROOT;?>/assets/images/loginlogo.jpg"  class="drloginlogo">
+            <span class="drlogin">Login</span>
+        </a>
+        <div id="drDropdownMenu" class="dr-dropdown-content">
+            <a href="<?php echo URLROOT;?>/drProfile">View Profile</a>
+            <a href="#">Logout</a>
+        </div>
+    </div>
 </div>
+
+<script>
+    function toggleDropdown() {
+        document.getElementById("drDropdownMenu").classList.toggle("show");
+    }
+
+    // Close the dropdown if the user clicks outside of it
+    window.onclick = function(event) {
+        if (!event.target.matches('.dr-profile-link') && !event.target.matches('.drloginlogo') && !event.target.matches('.drlogin')) {
+            var dropdowns = document.getElementsByClassName("dr-dropdown-content");
+            for (var i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
+</script>
 
 </body>
 </html>
