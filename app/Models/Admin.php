7e4e766d19de7e673129f69c3dcb9c6b326b775e
tharushasamarawickrama <140165950+tharushasamarawickrama@users.noreply.github.com>
2024-11-20 14:05:49 +0530

    create admin model

---
+++ app/Models/Admin.php
@@ -0,0 +1,34 @@
+<?php
+
+
+class Admin {
+    use Model;
+
+    protected $table = "admins";
+
+    protected $Allowedcolumns = [
+        'admin_id',
+        'firstName',
+        'lastName',
+        'password',
+        'phoneNumber',
+        'email',
+        'gender',
+        'dob',
+        'nic',
+        'address',
+        'photo_path',
+        'created_at'
+        
+    ];
+
+    public function findAlldata()
+    {
+
+        $query = "select * from $this->table ";
+
+        return $this->query($query);
+    }
+
+
+}
