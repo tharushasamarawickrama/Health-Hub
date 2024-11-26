aa5ad46fcdaac37ff2177389cd3a8b2709a7cada
tharushasamarawickrama <140165950+tharushasamarawickrama@users.noreply.github.com>
2024-11-26 16:10:04 +0530

    add users

--- app/controllers/DrProfile.php
+++ app/controllers/DrProfile.php
@@ -4,7 +4,7 @@ class DrProfile {
     use Controller;
 
     public function index() {
-        $doctorId = 5; // Hardcoded doctor ID (can be dynamic based on session/login)
+        $doctorId = 53; // Hardcoded doctor ID (can be dynamic based on session/login)
         if (isset($_SESSION['success_message'])) {
             echo "<script>alert('" . $_SESSION['success_message'] . "');</script>";
             unset($_SESSION['success_message']);
