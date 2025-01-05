f51e3255d4c0f444964f7c4164e20afe66974088
tharushasamarawickrama <140165950+tharushasamarawickrama@users.noreply.github.com>
2025-01-05 09:24:20 +0530

    edit

--- app/views/DrProfiledetails.view.php
+++ app/views/DrProfiledetails.view.php
@@ -8,7 +8,7 @@
           
         </div>
         <form class="DrProfiledetails-profile-details-form">
-            <div>
+            <div class="DrProfiledetails-profile-div">
                 <img src="<?php echo URLROOT; ?>/<?php echo $data['photo_path']?>" alt="Doctor's Photo" class="DrProfiledetails-profile-photo">
             </div>
             <div class="form-row">
