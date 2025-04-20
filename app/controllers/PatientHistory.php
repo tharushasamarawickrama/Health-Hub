98e09c2faf705f7bfc10396e34f16a4964555378
ASHAN2021 <akh8130@gmail.com>
2025-04-20 19:47:12 +0530

    labtest report  view implemented

--- app/controllers/PatientHistory.php
+++ app/controllers/PatientHistory.php
@@ -8,34 +8,39 @@ class PatientHistory
         $id = $_SESSION['user']['user_id'];
         $appointment = new Appointment;
         $appointments = $appointment->getAppointmentsByUserId($id);
-        if($appointments){
-            
+        if ($appointments) {
+
             foreach ($appointments as $appointment) { // Use reference to modify the original array
                 $prescription = new Prescription;
                 $labtest = new LabTest;
                 $prescriptionMed = new Prescribed_Medications;
+                $appointmentlabtest = new Appointment_LabTest;
                 $arr['prescription_id'] = $appointment['prescription_id'];
+                $arr1['labtest_id'] = $appointment['labtest_id'];
                 $prescriptiondata1 = $prescription->first($arr);
+                $labtestdata = $labtest->first($arr1);
+                $appointmentlabtestdata = $appointmentlabtest->getLabTestByAppointmentId($appointment['appointment_id']);
                 $prescriptiondata2 = $prescriptionMed->getmedicatesInprescription($appointment['prescription_id']);
                 $combinedData = [
                     'appointment' => $appointment,
                     'prescription' => $prescriptiondata1,
-                    'prescriptionMed' => $prescriptiondata2
+                    'prescriptionMed' => $prescriptiondata2,
+                    'labtest' => $labtestdata,
+                    'appointmentlabtest' => $appointmentlabtestdata,
                 ];
                 $data[] = $combinedData;
                 // show($data);  
-                
+
                 // Debugging output
-                
-                
+
+
                 // $labtestdata = $labtest->first(['labtest_id' => $appointment['labtest_id']]);
                 // $appointment['prescription'] = $prescriptiondata1;
                 // $appointment['labtest'] = $labtestdata;
             }
-            
         }
-        
-        
+
+
 
         $this->view('patienthistory', ['data' => $data]);
     }
--- app/views/labprocessedappointment.view.php
+++ app/views/labprocessedappointment.view.php
@@ -8,7 +8,7 @@
                     <img src="<?php echo URLROOT; ?>/assets/images/arrow-back.png" alt="Back" width="20px">
                     Back
                 </a>
-            </div> 
+            </div>
             <form action="<?php echo URLROOT; ?>/labprocessedappointment/markAsPending" method="POST">
                 <input type="hidden" name="appointment_id" value="<?php echo htmlspecialchars($data['appointment_id']); ?>">
                 <button type="submit" class="btn btn-warning">Mark as Pending</button>
@@ -36,18 +36,19 @@
             </div>
             <div class="lab-proc-app-uploaded-reports">
                 <h3>Uploaded reports</h3>
-                    <?php if (!empty($data['labtest_pdfname']) && !empty($data['labtest_report'])): ?>
-                    <?php 
+                <?php if (!empty($data['labtest_pdfname']) && !empty($data['labtest_report'])): ?>
+                    <?php
                     $pdfNames = explode(',', $data['labtest_pdfname']);
                     $pdfPaths = explode(',', $data['labtest_report']);
                     $labtest_id = explode(',', $data['labtest_id']);
                     ?>
-                <?php foreach ($pdfNames as $index => $pdfName): ?>
-                    <div class="lab-proc-app-report" id="report-<?php echo $index; ?>">
-                    <img src="<?php echo URLROOT; ?>/assets/images/pdf-icon.png" alt="PDF Icon"> 
-                    <span><?php echo htmlspecialchars($pdfName); ?></span>
-                    <a href="<?php echo URLROOT; ?>/<?php echo htmlspecialchars($pdfPaths[$index]); ?>" target="_blank">View</a>                </div>
-                    <button onclick="confirmDelete('<?php echo $labtest_id[$index]; ?>', '<?php echo $data['appointment_id']; ?>')">Delete</button>                    <?php endforeach; ?>
+                    <?php foreach ($pdfNames as $index => $pdfName): ?>
+                        <div class="lab-proc-app-report" id="report-<?php echo $index; ?>">
+                            <img src="<?php echo URLROOT; ?>/assets/images/pdf-icon.png" alt="PDF Icon">
+                            <span><?php echo htmlspecialchars($pdfName); ?></span>
+                            <a href="<?php echo URLROOT; ?>/<?php echo htmlspecialchars($pdfPaths[$index]); ?>" target="_blank">View</a>
+                        </div>
+                        <button onclick="confirmDelete('<?php echo $labtest_id[$index]; ?>', '<?php echo $data['appointment_id']; ?>')">Delete</button> <?php endforeach; ?>
                 <?php else: ?>
                     <p>No reports uploaded.</p>
                 <?php endif; ?>
@@ -60,4 +61,4 @@
     const URLROOT = "<?php echo URLROOT; ?>";
 </script>
 <?php require APPROOT . '/views/Components/footer.php'; ?>
-<script src="<?php echo URLROOT;?>/assets/js/LabProcessedAppointments.js"></script>
+<script src="<?php echo URLROOT; ?>/assets/js/LabProcessedAppointments.js"></script>
